2cf9aef10468d5957b8c4bf7ed96a2673f7ffbc9
Peeters, Stefano <6713256+PsySolix@users.noreply.github.com>
2021-12-15 16:53:12 +0100

    Update [slug].js
    Removed console.log

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -4,7 +4,6 @@ import CollectionService from "../../services/collection.service";
 import styles from "../../styles/Post.module.css";
 
 export default function Post({ post: { body, meta } }) {
-  console.log({ meta })
   return (
     <article>
       <h1>{meta.title}</h1>
